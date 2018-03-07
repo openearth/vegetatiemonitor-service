ce15e63e5bde115e8cceeab3f33daca7435afaa5
gena <gennadiy.donchyts@gmail.com>
2018-03-07 23:39:53 +0100

    implement first version of API using swagger / flasgger

--- app/main.py
+++ app/main.py
@@ -1,41 +1,87 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from flask import Flask, jsonify, request
+from flasgger import Swagger
 
-# [START app]
-import logging
+import ee
 
-from flask import Flask
+ee.Initialize()
 
 app = Flask(__name__)
+swagger = Swagger(app, template_file='api.yaml')
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+band_names = {
+    's2': ['B2', 'B3', 'B4', 'B8', 'B12'],
+    'readable': ['blue', 'green', 'red', 'nir', 'swir']
+}
 
+def get_sentinel_image(region, dateBegin, dateEnd, bands):
+    images = ee.ImageCollection('COPERNICUS/S2')\
+        .select(band_names['s2'], band_names['readable'])\
+        .filterBounds(region)
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+    if dateBegin:
+        if not dateEnd:
+            dateEnd = dateBegin.advance(1, 'day')
+
+        images = images.filterDate(dateBegin, dateEnd)
+
+    image = ee.Image(images.mosaic()).divide(10000)
+
+    vis = {'min': 0.05, 'max': [0.35, 0.35, 0.45], 'gamma': 2, 'bands': bands}
+
+    image = image.visualize(**vis)
+
+    return image
+
+def get_ndvi(region, dateBegin, dateEnd):
+    pass
+
+def get_landuse(region, dateBegin, dateEnd):
+    pass
+
+def get_landuse_vs_legger(region, dateBegin):
+    pass
+
+maps = {
+    'satellite': get_sentinel_image,
+    'ndvi': get_ndvi,
+    'landuse': get_landuse,
+    'landuse-vs-legger': get_landuse_vs_legger
+}
+
+@app.route('/map/<string:id>/', methods=['POST'])
+def get_map(id):
+    """
+    Returns maps processed by Google Earth Engine
+    """
+
+    json = request.get_json()
+
+    region = json['region']
+
+    dateBegin = json['dateBegin']
+    dateEnd= json['dateEnd']
+
+    dateBegin = dateBegin or ee.Date(dateBegin)
+    dateEnd = dateEnd or ee.Date(dateEnd)
+
+    bands = json['bands']
+
+    image = maps[id](region, dateBegin, dateEnd, bands)
+
+    map = image.getMapId()
+    map_id = map['mapid']
+    token = map['token']
+
+    url = 'https://earthengine.googleapis.com/map/'\
+        '{0}/{{z}}/{{x}}/{{y}}?token={1}'\
+        .format(map_id, token)
+
+    results = {'url': url}
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END app]
