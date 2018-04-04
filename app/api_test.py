9d6b8fc02037f52beb1be5f157e89ffa62498b2c
gena <gennadiy.donchyts@gmail.com>
2018-04-04 18:11:32 +0200

    * implemented zonal info for classified image (landuse) * fix warning (reformat)

--- app/api_test.py
+++ app/api_test.py
@@ -3,6 +3,7 @@ import pytest
 
 from . import main
 
+
 @pytest.fixture()
 def client():
     print(dir(main))
@@ -10,6 +11,7 @@ def client():
     main.app.testing = True
 
     return main.app.test_client()
+
 
 def test_get_zonal_info_legger(client):
     input = '''{
@@ -38,32 +40,56 @@ def test_get_zonal_info_legger(client):
         "scale": 100
         }'''
 
-    r = client.post('/map/legger/zonal-info/', data=input, content_type='application/json')
+    r = client.post('/map/legger/zonal-info/', data=input,
+                    content_type='application/json')
 
     assert r.status_code == 200
 
-    output = sorted(json.loads(r.get_data(as_text=True)))
+    s = r.get_data(as_text=True)
+    open('test_output_zonal_legger.json', 'w').write(s)
+
+    output = sorted(json.loads(s))
 
     output_expected = '''[
         {
            "id": 1,
-           "area_per_type": [
-                {"area": 567062.7642463235, "type": "0"}, 
-                {"area": 1624781.6086224725, "type": "1"}, 
-                {"area": 252953.091796875, "type": "2"}, 
-                {"area": 1854597.840469899, "type": "3"}, 
-                {"area": 682186.362109375, "type": "4"}, 
-                {"area": 382541.0576171875, "type": "5"}, 
-                {"area": 104886.63330078125, "type": "6"}
-           ]
+            "area_per_type": [
+              {
+                "area": 567062.7642463235, 
+                "type": "0"
+              }, 
+              {
+                "area": 1624781.6086224725, 
+                "type": "1"
+              }, 
+              {
+                "area": 252953.091796875, 
+                "type": "2"
+              }, 
+              {
+                "area": 1854597.840469899, 
+                "type": "3"
+              }, 
+              {
+                "area": 682186.362109375, 
+                "type": "4"
+              }, 
+              {
+                "area": 382541.0576171875, 
+                "type": "5"
+              }, 
+              {
+                "area": 104886.63330078125, 
+                "type": "6"
+              }
+            ] 
         }
     ]'''
 
     output_expected = sorted(json.loads(output_expected))
 
-    open('test_output_zonal_legger.json', 'w').write(json.dumps(output))
-
     assert output[0]["area_per_type"] == output_expected[0]["area_per_type"]
+
 
 def test_get_zonal_info_landuse(client):
     input = '''{
@@ -94,32 +120,56 @@ def test_get_zonal_info_landuse(client):
         "scale": 100
         }'''
 
-    r = client.post('/map/landuse/zonal-info/', data=input, content_type='application/json')
+    r = client.post('/map/landuse/zonal-info/', data=input,
+                    content_type='application/json')
 
     assert r.status_code == 200
 
-    output = sorted(json.loads(r.get_data(as_text=True)))
+    s = r.get_data(as_text=True)
+    open('test_output_zonal_landuse.json', 'w').write(s)
+
+    output = sorted(json.loads(s))
 
     output_expected = '''[
         {
             "id": 1, 
             "area_per_type": [
-                {"type": "", "area": 567062.7642463235}, 
-                {"type": "Water", "area": 1624781.6086224725}, 
-                {"type": "Verhard oppervlak", "area": 252953.091796875}, 
-                {"type": "Gras en Akker", "area": 1854597.840469899}, 
-                {"type": "Riet en Ruigte", "area": 682186.362109375}, 
-                {"type": "Bos", "area": 382541.0576171875}, 
-                {"type": "Struweel", "area": 104886.63330078125}
-            ]
+              {
+                "area": 133988.3464499081, 
+                "type": "0"
+              }, 
+              {
+                "area": 1406658.7865559896, 
+                "type": "1"
+              }, 
+              {
+                "area": 801080.4081954657, 
+                "type": "2"
+              }, 
+              {
+                "area": 2543533.6747740502, 
+                "type": "3"
+              }, 
+              {
+                "area": 404511.24248046876, 
+                "type": "4"
+              }, 
+              {
+                "area": 518283.17333984375, 
+                "type": "5"
+              }, 
+              {
+                "area": 141902.51318359375, 
+                "type": "6"
+              }
+            ] 
         }
     ]'''
 
     output_expected = sorted(json.loads(output_expected))
 
-    open('test_output_zonal_landuse.json', 'w').write(json.dumps(output))
-
     assert output[0]["area_per_type"] == output_expected[0]["area_per_type"]
+
 
 def test_get_map_landuse(client):
     input = '''{
@@ -138,7 +188,8 @@ def test_get_map_landuse(client):
                    }
     }'''
 
-    r = client.post('/map/landuse/', data=input, content_type='application/json')
+    r = client.post('/map/landuse/', data=input,
+                    content_type='application/json')
 
     assert r.status_code == 200
 
