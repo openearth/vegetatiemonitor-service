cbc3f2d6387250f63cb68c5abf9a555bb0d6b858
gena <gennadiy.donchyts@gmail.com>
2019-11-07 10:10:52 +0100

    added script to deploy test version

---
+++ deploy-test.cmd
@@ -0,0 +1,2 @@
+cd app
+gcloud app deploy --project vegetatie-monitor --version=v2-test --no-promote
--- deploy-test2.cmd
+++
@@ -1,2 +0,0 @@
-cd app
-gcloud app deploy --project vegetatie-monitor --version=v2-test2 --no-promote
