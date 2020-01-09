2f9fbe31f299b06489a14d786807eff393a5a359
gena <gennadiy.donchyts@gmail.com>
2020-01-09 17:42:03 +0100

    fix bug, images were not shown

--- app/api.py
+++ app/api.py
@@ -186,7 +186,7 @@ def visualize_image(image, vis):
 
 def get_satellite_image(region, date_begin, date_end, vis):
     images = get_satellite_images(region, date_begin, date_end, False)
-    image = ee.Image(images.first()).divide(10000)
+    image = ee.Image(images.mosaic()).divide(10000)
     image = visualize_image(image, vis)
 
     return image
